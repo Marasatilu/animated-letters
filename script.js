f9769ecf5fbb1ebfe1ab814579f57dce504c2c6d
marasatilu <marasatilu@gmail.com>
2024-07-17 14:06:06 +0200

    added a pop-up with an event listener on button

--- script.js
+++ script.js
@@ -19,4 +19,41 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.invertButton').addEventListener('click', function() {
         document.querySelector('.helloWorld .textContent').classList.toggle('mirror');
     });
+
+
+
+    //randomize color
+
+    document.querySelector('.randomizeButton').addEventListener('click', function() {
+        const randomizeColorDiv = document.querySelector('.randomizeColor');
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        randomizeColorDiv.style.color = randomColor;
+    });
+
+    //coffepop-up
+
+    const coffeeButton = document.querySelector('.coffeeButton');
+    const popup = document.querySelector('.popup');
+    const popupOverlay = document.querySelector('.popup-overlay');
+    const closePopup = document.querySelector('.popup .close');
+    const emailButton = document.querySelector('.emailButton');
+
+    coffeeButton.addEventListener('click', function() {
+        popup.style.display = 'block';
+        popupOverlay.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function() {
+        popup.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    popupOverlay.addEventListener('click', function() {
+        popup.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    emailButton.addEventListener('click', function() {
+        window.location.href = 'mailto:marasatilu@gmail.com?subject=Invitation for Coffee&body=Hi Mara,';
+    });
 });
