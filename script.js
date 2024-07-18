23e49763b96fc1b5959b1010e91db012d5097681
marasatilu <marasatilu@gmail.com>
2024-07-18 13:26:16 +0200

    add animations

--- script.js
+++ script.js
@@ -8,6 +8,62 @@ document.addEventListener('DOMContentLoaded', () => {
     const panicButton = document.querySelector('.panicButton'); // Selecciona el botón Panic Button
     const undoButton = document.querySelector('.undoButton'); // Selecciona el botón de Undo
     
+
+    spans.forEach(span => {
+        span.addEventListener('mouseover', () => {
+            span.classList.add('bounce');
+        });
+
+        span.addEventListener('animationend', () => {
+            span.classList.remove('bounce');
+        });
+    });
+
+    // Mirror effect
+    document.querySelector('.invertButton').addEventListener('click', function() {
+        document.querySelector('.helloWorld .textContent').classList.toggle('mirror');
+    });
+
+    // Randomize color
+    document.querySelector('.randomizeButton').addEventListener('click', function() {
+        const randomizeColorDiv = document.querySelector('.randomizeColor');
+        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        randomizeColorDiv.style.color = randomColor;
+    });
+
+    // Coffee pop-up
+    const coffeeButton = document.querySelector('.coffeeButton');
+    const popup = document.querySelector('.popup');
+    const popupOverlay = document.querySelector('.popup-overlay');
+    const closePopup = document.querySelector('.popup .close');
+    const emailButton = document.querySelector('.emailButton');
+
+    coffeeButton.addEventListener('click', function() {
+        popup.style.display = 'flex';
+        popupOverlay.style.display = 'block';
+    });
+
+    closePopup.addEventListener('click', function() {
+        popup.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    popupOverlay.addEventListener('click', function() {
+        popup.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    emailButton.addEventListener('click', function() {
+        window.location.href = 'mailto:marasatilu@gmail.com?subject=Invitation for Coffee&body=Hi Mara,';
+    });
+
+
+
+
+
+
+
+
     // Función para añadir clase 'fall' a elementos
     function addFallClass() {
         spans.forEach(span => {
